2feee5444648db65484392528ae7952b04bdaecc
trietnguyen0711 <nguyenminhtriet07112008@gmail.com>
2024-06-12 15:23:27 +0700

    Count

--- style.js
+++ style.js
@@ -1,24 +1,67 @@
-let x = 0
 let CountJs = document.getElementById("Count")
-function increase() {
-    x = x + 1
+if (localStorage.getItem("points")) {
+    let x = parseInt(localStorage.getItem("points"))
     CountJs.innerText = x
+    localStorage.setItem("points", x)
 }
-function decrease() {
-    if (x == 0) {
-        alert("Don't have any people to decrease")
+
+function increase() {
+    if (localStorage.getItem("points")) {
+        let x = parseInt(localStorage.getItem("points"))
+        x = x + 1
+        CountJs.innerText = x
+        localStorage.setItem("points", x)
     }
     else {
-        x = x - 1
+        let x = 0
+        x = x + 1
         CountJs.innerText = x
+        localStorage.setItem("points", x)
+    }
+}
+function decrease() {
+    if (localStorage.getItem("points")) {
+        let x = parseInt(localStorage.getItem("points"))
+        if (x == 0) {
+            alert("Don't have any points to decrease")
+        }
+        else {
+            x = x - 1
+            CountJs.innerText = x
+            localStorage.setItem("points", x)
+        }
+    }
+    else {
+        let x = 0
+        if (x == 0) {
+            alert("Don't have any points to decrease")
+        }
+        else {
+            x = x - 1
+            CountJs.innerText = x
+        }
     }
 }
 function reset() {
-    if (x == 0) {
-        alert("It has already reseted")
+    if (localStorage.getItem("points")) {
+        let x = parseInt(localStorage.getItem("points"))
+        if (x == 0) {
+            alert("It has already reseted")
+        }
+        else {
+            x = 0
+            CountJs.innerText = x
+            localStorage.setItem("points", x)
+        }
     }
     else {
-        x = 0
-        CountJs.innerText = x
+        let x = 0
+        if (x == 0) {
+            alert("It has already reseted")
+        }
+        else {
+            x = 0
+            CountJs.innerText = x
+        }
     }
 }
